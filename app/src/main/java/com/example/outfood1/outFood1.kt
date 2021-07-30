221f29e92987246bb73a55be2584dcb7c0395ee4
sayone99 <65717358+sayone99@users.noreply.github.com>
2021-07-30 09:51:15 +0900

    initiate commit

---
+++ app/src/main/java/com/example/outfood1/outFood1.kt
@@ -0,0 +1,13 @@
+package com.example.outfood1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class outFood1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
