5f14399cf1f946a41a55e3775d87e63ab6388baf
sayone99 <65717358+sayone99@users.noreply.github.com>
2021-08-01 11:58:49 +0900

    Update outfood3reviewwrite file

--- app/src/main/java/com/example/outfood1/outFood3.kt
+++ app/src/main/java/com/example/outfood1/outFood3.kt
@@ -19,10 +19,10 @@ class outFood3 : AppCompatActivity() {
 
         val adapter = outFood3ReviewAdapter() // (어댑터 생성)
 
-        adapter.items.add(outFood3Review("익명","4.0","매운 떡볶이 땡기는 날엔 강추합니다! 근데 실내가 너무 덥습니다."))
-        adapter.items.add(outFood3Review("익명", "5.0","굉장히 맛있어요! 사장님도 정말 착하십니다!"))
-        adapter.items.add(outFood3Review("익명", "3.0","알바생이 부족해보입니다. 웨이팅도 길었구요. 맛은 소문대로 맛있었습니다."))
-        adapter.items.add(outFood3Review("익명", "4.5","친구랑 갔는데 둘 다 너무 만족했습니다! 다음에 또 오고 싶어요! 특히 치즈가 맛있습니다~"))
+        adapter.items.add(outFood3Review("0", "익명",4,"매운 떡볶이 땡기는 날엔 강추합니다! 근데 실내가 너무 덥습니다."))
+        adapter.items.add(outFood3Review("1", "익명", 5,"굉장히 맛있어요! 사장님도 정말 착하십니다!"))
+        adapter.items.add(outFood3Review("2", "익명", 3,"알바생이 부족해보입니다. 웨이팅도 길었구요. 맛은 소문대로 맛있었습니다."))
+        adapter.items.add(outFood3Review("3", "익명", 4,"친구랑 갔는데 둘 다 너무 만족했습니다! 다음에 또 오고 싶어요! 특히 치즈가 맛있습니다~"))
         // 2. 어댑터에 데이터 저장
         // 익명
         recyclerView.adapter = adapter
@@ -30,7 +30,7 @@ class outFood3 : AppCompatActivity() {
 
         outFood3ReviewBtn.setOnClickListener {
             val intent = Intent(this, outFood3ReviewWrite::class.java)
-            startActivityForResult(intent, 102)
+            startActivityForResult(intent,101)
         }
     }
 }
--- app/src/main/java/com/example/outfood1/outFood3ReviewWrite.kt
+++ app/src/main/java/com/example/outfood1/outFood3ReviewWrite.kt
@@ -8,38 +8,37 @@ import kotlinx.android.synthetic.main.activity_out_food3_reviewwrite.*
 
 class outFood3ReviewWrite : AppCompatActivity() {
 
-    private lateinit var databaseRef: DatabaseReference
+//    private lateinit var databaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_out_food3_reviewwrite)
 
-        outFood3ReviewWriteSave.setOnClickListener {
-            val name = outFood3ReviewWriteName.text.toString()
-            val rating = outFoodStar.rating.toString()
-            val contents = outFood3ReviewWriteCont.text.toString()
+//        outFood3ReviewWriteSave.setOnClickListener {
+//            val name = outFood3ReviewWriteName.text.toString()
+//            val rating = outFoodStar.rating.toString().toLong()
+//            val contents = outFood3ReviewWriteCont.text.toString()
+//
+//            saveComment(name, rating ,contents)
+//        }
+//
+//    }
 
-            saveComment(name,rating,contents)
-        }
-
-        fun saveComment (name:String, rating:Long,contents:String) {
-            val key:String? = databaseRef.child("comments").push().getKey()
-            val comment = FoodComment(key!!, name, rating, contents)
-            val commentValues: HashMap<String, Any> = comment.toMap()
-
-            val childUpdates: MutableMap<String, Any> = HashMap()
-            childUpdates["/comments/$key"] = commentValues
-
-            databaseRef.updateChildren(childUpdates)
-
-        databaseRef = FirebaseDatabase.getInstance().reference
-
-
-
-
-    }
-
+//    fun saveComment (name:String, rating:Long, contents:String) {
+//        val key:String? = databaseRef.child("comments").push().key
+//        val comment = outFood3Review(key!!, name, rating, contents)
+//        // 책에서 쓴 objectid는 객체의 key를 의미하는 것이었음. 우리도 객체를 구분할 키값을 담을 데이터 인수를 하나 더 설정해야 함
+//        // 그래서 setitem에서도 objectid에 대한 언급이 없었던 것.
+//        val commentValues: HashMap<String?, Any?> = comment.toMap()
+//
+//        val childUpdates: MutableMap<String, Any> = HashMap()
+//        childUpdates["/comments/$key"] = commentValues
+//
+//        databaseRef.updateChildren(childUpdates)
+//
+//        databaseRef = FirebaseDatabase.getInstance().reference
 
 
+//    }
     }
 }
